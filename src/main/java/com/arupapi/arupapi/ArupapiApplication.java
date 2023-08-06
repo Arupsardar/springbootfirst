23d5d592c989ffcf8b0d479df61e4a0aa08085e0
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2023-08-06 11:52:17 +0530

    springboot first push

---
+++ src/main/java/com/arupapi/arupapi/ArupapiApplication.java
@@ -0,0 +1,13 @@
+package com.arupapi.arupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArupapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArupapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arupapi/arupapi/ArupapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arupapi.arupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArupapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
